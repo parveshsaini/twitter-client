ad1f1f701b10a07f6b857d7659fb5213089fd908
parveshsaini <parveshsaini619@gmail.com>
2025-06-26 23:47:30 +0530

    revive

--- codegen.ts
+++ codegen.ts
@@ -3,8 +3,8 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  // schema: "https://twittercloneparvesh.twilightparadox.com/graphql",
-  schema: "http://localhost:3000/graphql",
+  schema: "https://twitter-api.parveshsaini.com/graphql",
+  // schema: "http://localhost:8080/graphql",
   documents: "**/*.{tsx,ts}",
   generates: {
     "gql/": {
